5c15b958001c6481b97754d73220f5d53ee27311
shocks13 <shocks13@gmail.com>
2016-11-16 19:44:19 +0000

    added onBoard1

--- modules/handlers.js
+++ modules/handlers.js
@@ -57,89 +57,79 @@ exports.button3 = (sender) => {
 exports.button4 = (sender) => {
     console.log('button4 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q2: `Colocataire`}, sender).then(() => {
-            messenger.send(formatter.onBoard3(response), sender);
-        });
+        messenger.send(formatter.onBoard3(response), sender);
+        salesforce.updateLead({q2: `Colocataire`}, sender).then(() => {});
     });
 };
 
 exports.button5 = (sender) => {
     console.log('button5 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q2: `Propriétaire`}, sender).then(() => {
-            messenger.send(formatter.onBoard3(response), sender);
-        });
+        messenger.send(formatter.onBoard3(response), sender);
+        salesforce.updateLead({q2: `Propriétaire`}, sender).then(() => {});
     });
 };
 
 exports.button6 = (sender) => {
     console.log('button6 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q3: `Alarme incendie`}, sender).then(() => {
-            messenger.send(formatter.onBoard4(response), sender);
-        });
+        messenger.send(formatter.onBoard4(response), sender);
+        salesforce.updateLead({q3: `Alarme incendie`}, sender).then(() => {});
     });
 };
 
 exports.button7 = (sender) => {
     console.log('button7 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q3: `Objets connectés`}, sender).then(() => {
-            messenger.send(formatter.onBoard4(response), sender);
-        });
+        messenger.send(formatter.onBoard4(response), sender);
+        salesforce.updateLead({q3: `Objets connectés`}, sender).then(() => {});
     });
 };
 
 exports.button8 = (sender) => {
     console.log('button8 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q3: `Alarme effraction`}, sender).then(() => {
-            messenger.send(formatter.onBoard4(response), sender);
-        });
+        messenger.send(formatter.onBoard4(response), sender);
+        salesforce.updateLead({q3: `Alarme effraction`}, sender).then(() => {});
     });
 };
 
 exports.button9 = (sender) => {
     console.log('button9 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q4: `Pas d'assureur`}, sender).then(() => {
-            messenger.send(formatter.onBoard5(response), sender);
-        });
+        messenger.send(formatter.onBoard5(response), sender);
+        salesforce.updateLead({q4: `Pas d'assureur`}, sender).then(() => {});
     });
 };
 
 exports.button10 = (sender) => {
     console.log('button10 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q4: `AXA`}, sender).then(() => {
-            messenger.send(formatter.onBoard5(response), sender);
-        });
+        messenger.send(formatter.onBoard5(response), sender);
+        salesforce.updateLead({q4: `AXA`}, sender).then(() => {});
     });
 };
 
 exports.button11 = (sender) => {
     console.log('button11 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q4: `MAIF`}, sender).then(() => {
-            messenger.send(formatter.onBoard5(response), sender);
-        });
+        messenger.send(formatter.onBoard5(response), sender);
+        salesforce.updateLead({q4: `MAIF`}, sender).then(() => {});
     });
 };  
 
 exports.button12 = (sender) => {
     console.log('button12 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q4: `CNP Assurances`}, sender).then(() => {
-            messenger.send(formatter.onBoard5(response), sender);
-        });
+        messenger.send(formatter.onBoard5(response), sender);
+        salesforce.updateLead({q4: `CNP Assurances`}, sender).then(() => {});
     });
 };  
 
 exports.button13 = (sender) => {
     console.log('button13 called');
     messenger.getUserInfo(sender).then(response => {
-        salesforce.updateLead({q4: `Autre`}, sender).then(() => {
-            messenger.send(formatter.onBoard5(response), sender);
-        });
+        messenger.send(formatter.onBoard5(response), sender);
+        salesforce.updateLead({q4: `Autre`}, sender).then(() => {});
     });
 };
