5dce4d5fa4d0e07493cace204ccef36f6b5989e9
shocks13 <shocks13@gmail.com>
2017-01-30 18:05:19 +0000

    changes

--- modules/formatter.js
+++ modules/formatter.js
@@ -414,6 +414,28 @@ exports.formatRecommendation = rec => {
                 ]
             },
             {
+                title: 'Cumulus Confort',
+                subtitle: `17,99€ par mois`,
+                "image_url": 'https://drive.google.com/uc?export=view&id=0BxwASYlURQ-JWHdVeWM2dVc5Q2o0dzZVMUJVVGE5NGg3MF84',
+                "buttons": [
+                    {
+                        "type":"web_url",
+                        "url":"https://www.facebook.com/cumulusfrance",
+                        "title":"Avis Communauté"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Rdv conseiller",
+                        "payload": "schedule_visit"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Souscrire",
+                        "payload": "image_postback"
+                    }
+                ]
+            },
+            {
                 title: 'Cumulus Confort Plus',
                 subtitle: `20,99€ par mois`,
                 "image_url": 'https://drive.google.com/uc?export=view&id=0BxwASYlURQ-JLWRiUjBPejFHREh3dnFZMjNRbzh2U2hyOE9V',
@@ -434,31 +456,7 @@ exports.formatRecommendation = rec => {
                         "payload": "image_postback"
                     }
                 ]
-            },
-            {
-                title: 'Offre maison connectée',
-                subtitle: `17,99€ par mois`,
-                "image_url": 'https://drive.google.com/uc?export=view&id=0BxwASYlURQ-JOEpjd0wxRTllZUpaNnJWWnpfNTQ2S2gxbHc0',
-                "buttons": [
-                    {
-                        "type":"web_url",
-                        "url":"https://www.facebook.com/cumulusfrance",
-                        "title":"Avis Communauté"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Rdv conseiller",
-                        "payload": "schedule_visit"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Souscrire",
-                        "payload": "image_postback"
-                    }
-                ]
             }
-
-
         );
 
     return {
