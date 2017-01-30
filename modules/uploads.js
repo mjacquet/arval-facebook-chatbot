098ed745f720d4535ff0a8b349cfe401995a8b82
shocks13 <shocks13@gmail.com>
2017-01-30 14:59:00 +0000

    changes

--- modules/uploads.js
+++ modules/uploads.js
@@ -40,11 +40,13 @@ exports.processUpload = (sender, attachments) => {
             geocoder.reverse({lat: attachment.payload.coordinates.lat, lon: attachment.payload.coordinates.long}).then(function(res) {
                 console.log('result: ', res);
                 console.log('ZIPCODE!: ', res[0].zipcode);
-
+                
+                /*
                 messenger.getSuggestion(res[0].zipcode, '2').then(response => {
                     messenger.send({text: `${response.service_plan}`}, sender);
                 });
-
+                */
+                
             }).catch(function(err) {
                 console.log('err: ', err);
             });
