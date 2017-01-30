315e7eb53c4a23d05dd8bd17291057293b734d48
shocks13 <shocks13@gmail.com>
2017-01-30 17:55:00 +0000

    changes

--- modules/formatter.js
+++ modules/formatter.js
@@ -387,5 +387,90 @@ exports.renderRooms = response => {
     }
 };
 
+exports.formatRecommendation = rec => {
+
+    let elements = [];
+        elements.push(  
+            {
+                title: rec.get("Name"),
+                subtitle: `${rec.get("subtitle__c")}`,
+                "image_url": rec.get("image__c"),
+                "buttons": [
+                    {
+                        "type":"web_url",
+                        "url":"https://www.facebook.com/cumulusfrance",
+                        "title":"Avis Communauté"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Rdv conseiller",
+                        "payload": "schedule_visit"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Souscrire",
+                        "payload": "image_postback"
+                    }
+                ]
+            },
+            {
+                title: 'Cumulus Confort Plus',
+                subtitle: `20,99€ par mois`,
+                "image_url": 'https://drive.google.com/uc?export=view&id=0BxwASYlURQ-JLWRiUjBPejFHREh3dnFZMjNRbzh2U2hyOE9V',
+                "buttons": [
+                    {
+                        "type":"web_url",
+                        "url":"https://www.facebook.com/cumulusfrance",
+                        "title":"Avis Communauté"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Rdv conseiller",
+                        "payload": "schedule_visit"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Souscrire",
+                        "payload": "image_postback"
+                    }
+                ]
+            },
+            {
+                title: 'Offre maison connectée',
+                subtitle: `17,99€ par mois`,
+                "image_url": 'https://drive.google.com/uc?export=view&id=0BxwASYlURQ-JOEpjd0wxRTllZUpaNnJWWnpfNTQ2S2gxbHc0',
+                "buttons": [
+                    {
+                        "type":"web_url",
+                        "url":"https://www.facebook.com/cumulusfrance",
+                        "title":"Avis Communauté"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Rdv conseiller",
+                        "payload": "schedule_visit"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Souscrire",
+                        "payload": "image_postback"
+                    }
+                ]
+            }
+
+
+        );
+
+    return {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": elements
+            }
+        }
+    };
+};
+
 
  
