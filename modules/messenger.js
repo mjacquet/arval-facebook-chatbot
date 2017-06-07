5a873c839b906b81c0e4e7ceb7d6c3187a6a30c8
shocks13 <shocks13@gmail.com>
2017-06-07 14:58:10 +0100

    pio integration

--- modules/messenger.js
+++ modules/messenger.js
@@ -4,17 +4,25 @@ let request = require('request'),
     FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN,
     moment = require("moment"),
     numeral = require("numeral"),
-    nodeGeocoder = require('node-geocoder');
+    nodeGeocoder = require('node-geocoder'),
+    Forecast = require('forecast');
 
-let util = require('util')
-
-let weather = {};
-
+let util = require('util');
+// Initialize 
+var forecast = new Forecast({
+    service: 'darksky',
+    key: '784aaa420a59c6ac369b1d6c7a369752',
+    units: 'celcius',
+    cache: true,      // Cache API requests 
+    ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/ 
+        minutes: 30
+    }
+});
 var options = {
       provider: 'google'
 };
-
 var geocoder = nodeGeocoder(options);
+let weather = {};
 
 exports.setWeather = (params) => {
     return new Promise((resolve, reject) => {
@@ -32,6 +40,14 @@ exports.setWeather = (params) => {
         else if(params[0] == "confirm_visit"){
             weather.datetime = params[1];
             weather.jsdate = moment(params[1], "ddd Do MMM [at] ha");
+            forecast.get([weather.latitude, weather.longitude], function(err, weather) {
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log(weather);
+                }
+            });
         }
 
         console.log("setWeather: ", weather);
